680b1fbf089f25d23f126cc407722ed677668d16
Ash W <amsheppard2016@gmail.com>
2020-06-22 22:49:04 -0400

    added pg and fixed knexfile to deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,6 @@
 // Update with your config settings.
+const pgConnection =
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
 
 module.exports = {
     development: {
@@ -20,35 +22,32 @@ module.exports = {
         },
     },
 
-    // staging: {
-    //     client: "postgresql",
-    //     connection: {
-    //         database: "my_db",
-    //         user: "username",
-    //         password: "password",
-    //     },
-    //     pool: {
-    //         min: 2,
-    //         max: 10,
-    //     },
-    //     migrations: {
-    //         tableName: "knex_migrations",
-    //     },
-    // },
+    testing: {
+        client: "sqlite3",
+        connection: {
+            filename: "./database/auth.db3",
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: "./database/migrations",
+        },
+        seeds: {
+            directory: "./database/seeds",
+        },
+    },
 
-    // production: {
-    //     client: "postgresql",
-    //     connection: {
-    //         database: "my_db",
-    //         user: "username",
-    //         password: "password",
-    //     },
-    //     pool: {
-    //         min: 2,
-    //         max: 10,
-    //     },
-    //     migrations: {
-    //         tableName: "knex_migrations",
-    //     },
-    // },
+    production: {
+        client: "pg",
+        connection: { pgConnection },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+        seeds: {
+            directory: "./database/seeds",
+        },
+    },
 };
