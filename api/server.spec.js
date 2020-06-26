511466f7168f38d624f3ae9b99b3af13c84c27db
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-06-25 21:45:02 -0500

    added tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,4 +5,51 @@ describe('server.js', () => {
     it('should set the testing environment', () => {
         expect(process.env.DB_ENV).toBe('testing');
     });
+
+    // return 200 status on GET /api/recipes
+    describe('GET /api/recipes', () => {
+        it('should return a 200 status', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        });
+    })
+
+    // return 200 status on GET /api/recipes/:id
+    describe('GET /api/recipes/1', () => {
+        it('should return a 200 status', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        });
+    })
+
+    // return 200 status on GET /api/recipes/:id/instructions
+    describe('GET /api/recipes/1/instructions', () => {
+        it('should return a 200 status', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        });
+    })
+
+    // return 200 status on GET /api/recipes/:id/ingredients
+    describe('GET /api/recipes/1/ingredients', () => {
+        it('should return a 200 status', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        });
+    })
+
+    // return "Server is running"
+    // describe('GET /', () => {
+    //     it('should return "Server is running"', () => {
+    //         return(server).get('/');
+    //     })
+    // })
 })
